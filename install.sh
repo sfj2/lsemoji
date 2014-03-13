897e8a095d23dd6aa2efb9b8641f786bcb4a262a
David Miller <davidfmiller@me.com>
2014-03-13 14:37:51 -0700

    tweaked install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/bin
-echo -e "\033[1;34mDownloading shell script ...\033[0m"
+echo "🌍  Downloading lsemoji install script from https://github.com/davidfmiller/lsemoji..."
 
 curl –silent https://raw.github.com/davidfmiller/lsemoji/master/lsemoji.py > ~/bin/lsemoji > /dev/null 2>&1
 chmod +x ~/bin/lsemoji
 
-echo -e "\033[1;32mDone!\033[0m"
-echo -e "\033[1;33mMake sure ~/bin is in your PATH to have lsemoji available\033[0m"
+echo "👍  lsemoji is in ~/bin"
+
